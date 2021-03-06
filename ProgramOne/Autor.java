e73f8a74e9b9bf8cd602fc7777c43d95fc79b70e
karenmentges <karen_ruvermentges@hotmail.com>
2021-03-06 19:32:16 -0300

    Changes

---
+++ ProgramOne/Autor.java
@@ -0,0 +1,7 @@
+/* Dado o código da classe abaixo, faça um programa que instancie um objeto Autor e leia do teclado os dados
+necessários para preencher todos os atributos do objeto instanciado. */
+
+class Autor {
+    String nome;
+    int id;
+}
---
+++ ProgramOne/ProgramOne.java
@@ -0,0 +1,26 @@
+/* Dado o código da classe abaixo, faça um programa que instancie um objeto Autor e leia do teclado os dados
+necessários para preencher todos os atributos do objeto instanciado. */
+
+import java.util.Scanner; 
+
+
+public class ProgramOne {
+    public static void main(String args[]) {
+
+        Autor autor = new Autor();;
+        
+        Scanner scanner;
+        scanner = new Scanner(System.in);
+
+        scanner.nextLine();
+        System.out.println("Digite o nome do Autor: ");
+        autor.nome = scanner.nextLine(); //fgets
+        
+        System.out.println("Digite o ID do Autor: ");
+        autor.id = scanner.nextInt(); // scanf
+
+        System.out.println("Nome: " + autor.nome);
+        System.out.println("ID: " + autor.id);
+   
+    }
+}
