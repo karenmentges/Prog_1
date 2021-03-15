4babea9be483d7b4b782305f00d9fa7410d659dc
karenmentges <karen_ruvermentges@hotmail.com>
2021-03-15 16:17:51 -0300

    changes

--- Lista 2/Atividade 3/Televisao.java
+++ Lista 2/Atividade 3/Televisao.java
@@ -1,12 +1,3 @@
-/* Crie uma classe Televisao e uma classe ControleRemoto que pode controlar o
-volume e trocar os canais da televisão. O controle de volume permite:
-● aumentar ou diminuir a potência do volume de som em uma unidade de cada vez;
-● aumentar e diminuir o número do canal em uma unidade;
-● trocar para um canal indicado;
-● consultar o valor do volume de som e o canal selecionado.
-Crie uma classe principal que permita ao usuário inserir as informações para a televisão, e
-utilizar os métodos do controle remoto. */
-
 public class Televisao {
     int volume, canal;
 
