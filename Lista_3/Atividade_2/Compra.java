f27914918d3902d46c01374f3ed2a5cb10d75d48
karenmentges <karen_ruvermentges@hotmail.com>
2021-04-05 16:10:09 -0300

    Changes

---
+++ Lista_3/Atividade_2/Compra.java
@@ -0,0 +1,23 @@
+class Compra {
+    private String nomeProduto;
+    private float preço;
+    private int quantidade;
+
+    public Compra(String produto, float preço, int quantidade) {}
+
+    public void setNomeProduto(String novo) {}
+
+    public String getNomeProduto() {}
+
+    public void setPreço(float preço) {}
+
+    public float getPreço() {}
+
+    public void setQuantidade(int quantidade) {}
+
+    public int getQuantidade() {}
+
+    // Imprime todas as propriedades da classe
+    public void imprimeResumo() {}
+
+}
