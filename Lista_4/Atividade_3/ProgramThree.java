ffb21d01bdeeb2c5e1c5d68ba04d90127d84fc16
karenmentges <karen_ruvermentges@hotmail.com>
2021-04-27 11:57:22 -0300

    Changes

--- Lista_4/Atividade_3/ProgramThree.java
+++ Lista_4/Atividade_3/ProgramThree.java
@@ -1,7 +1,9 @@
 public class ProgramThree {
     
     public static void main(String args[]){
-        Character a = new Character("Oi", true, "player1.png", 20, 20);
+        Character a = new Character("um", true, "player1.png", 20, 20);
+        Character b = new Character("dois", true, "another.png", -40, 13);
+        Character c = new Character("tres", true, "newone.png", 10, 5);
     }
     
 }
