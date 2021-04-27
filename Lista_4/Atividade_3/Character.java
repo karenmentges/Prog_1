9611273ab4c7e4c7eab6a1fbe2c297c142cb70ef
karenmentges <karen_ruvermentges@hotmail.com>
2021-04-27 11:32:56 -0300

    Changes

---
+++ Lista_4/Atividade_3/Character.java
@@ -0,0 +1,10 @@
+public class Character extends FlxSprite {
+
+    public Character(String j, boolean b, String n, int x, int y){
+        super(j);
+        setAlive(b);
+        loadGraphics(n);
+        reset(x, y);
+    }
+    
+}
