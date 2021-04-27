4a65aa6e19b4a9aaccc1e61db18c80829bb3efce
karenmentges <karen_ruvermentges@hotmail.com>
2021-04-27 14:55:42 -0300

    Changes

--- Lista_4/Atividade_1/ProgramOne.java
+++ Lista_4/Atividade_1/ProgramOne.java
@@ -1,7 +1,27 @@
 public class ProgramOne {
 
     public static void main(String args[]){
-       
+        Inimigo[] inimigo = new Inimigo[5];
+        Forca forca = new Forca();
+        Posicao posicao = new Posicao();
+        Services service = new Services();
+        int x = 40;
+
+        forca.setA(2000);
+        forca.setB(10);
+        posicao.setX(102);
+        posicao.setY(150);
+
+        for(int i=0; i<5; i++){
+            inimigo[i] = new Inimigo();
+            inimigo[i].setEnergia(x);
+            inimigo[i].setVelocidade(forca);
+            inimigo[i].setPosicao(posicao);
+            x = x + 20;
+        }
+        
+        service.imprime(inimigo);
+      
     }
    
 }
--- Lista_4/Atividade_1/Services.java
+++ Lista_4/Atividade_1/Services.java
@@ -1,5 +1,24 @@
 public class Services extends Inimigo{ 
     public static int contaPossiveisCandidatos(Inimigo i[]){
-        
+        int cont = 0;
+        Posicao p = new Posicao();
+        Forca f = new Forca();
+
+        for(int j=0; j<i.length; j++){
+            p = i[j].getPosicao();
+            f = i[j].getVelocidade();
+
+            if(p.getX()>100 || p.getY()>100 || p.getX()>100 && p.getY()>100){
+                if(i[j].getEnergia()<=50 && f.tamanho()==200){
+                    cont++;
+                }
+            }
+        }
+
+        return cont;
+    }
+
+    public void imprime(Inimigo i[]){
+        System.out.println(contaPossiveisCandidatos(i));
     }
 }
